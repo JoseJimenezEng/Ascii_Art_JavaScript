df1c7cadb1255b28bf1044398510aebfe501a69c
JoseJimenezEng <jjmarts@gmail.com>
2024-06-12 23:10:35 -0500

    Cambio doble ciclo for a un ciclo con index flattening -  simplificar finalimg+= "\n"; y finalimg += characters[charIndex]; en una sola línea.

--- imgtoascii.js
+++ imgtoascii.js
@@ -1,42 +1,51 @@
- let imagen;
- let canvas = document.createElement('canvas');
- let context = canvas.getContext('2d');
- let finalimg = "";
- let Px = 200;
- let Py = 100;
- 
- window.loadFile = function (event) {
-     imagen = document.getElementById('output');
-     imagen.style.filter = 'grayscale(1)';
-     imagen.src = URL.createObjectURL(event.target.files[0]);
+let imagen;
+let canvas = document.createElement('canvas');
+let context = canvas.getContext('2d');
+let finalimg = "";
+const Px = 200;
+const Py = 100;
 
-     imagen.onload = function () {
-         canvas.width = Px;
-         canvas.height = Py;
-         context.drawImage(imagen, 0, 0, Px, Py);
-         img2ascii(Px, Py);
-     };
- };
+loadFile = function (event) {
+    imagen = document.getElementById('output');
+    imagen.style.filter = 'grayscale(1)';
+    imagen.src = URL.createObjectURL(event.target.files[0]);
 
- function img2ascii(width, height) {
-     let characters = "@%#*+=-:. ";
-     const size = Math.round(255 / characters.length);
-     finalimg = "";
+    imagen.onload = function () {
+        canvas.width = Px;
+        canvas.height = Py;
+        context.drawImage(imagen, 0, 0, Px, Py);
+        img2ascii(Px, Py);
+    };
+};
 
-     for (let y = 0; y < height; y++) {
-         for (let x = 0; x < width; x++) {
-             let data = context.getImageData(x, y, 1, 1).data;
-             let grayscale = 0.2126 * data[0] + 0.7152 * data[1] + 0.0722 * data[2];
+function img2ascii(width, height) {
+    let characters = "@%#*+=-:. ";
+    const size = Math.round(255 / characters.length);
+    finalimg = "";
 
-             let charIndex = Math.floor(grayscale / size);
-             finalimg += characters[charIndex];
-         }
-         finalimg += "\n"; 
-     }
-     getFinalImg();
- }
+    //FORMA DOBLE FOR
+    // for (let y = 0; y < height; y++) {
+    //     for (let x = 0; x < width; x++) {
+    //         let data = context.getImageData(x, y, 1, 1).data;
+    //         let grayscale = 0.2126 * data[0] + 0.7152 * data[1] + 0.0722 * data[2];
+    //         let charIndex = Math.floor(grayscale / size);
+    //         finalimg += characters[charIndex];
+    //     }
+    //     finalimg += "\n";
+    // }
 
- function getFinalImg() {
-     let ascii = document.getElementById("imgascii");
-     ascii.textContent = finalimg;
- }
+    for (let k = 0; k < height * width; k++) {
+        let y = Math.floor(k / width);
+        let x = k % width;
+        let data = context.getImageData(x, y, 1, 1).data;
+        let grayscale = 0.2126 * data[0] + 0.7152 * data[1] + 0.0722 * data[2];
+        let charIndex = Math.floor(grayscale / size);
+        finalimg += (x < width - 1) ? characters[charIndex] : "\n";
+    }
+    getFinalImg();
+}
+
+function getFinalImg() {
+    let ascii = document.getElementById("imgascii");
+    ascii.textContent = finalimg;
+}
